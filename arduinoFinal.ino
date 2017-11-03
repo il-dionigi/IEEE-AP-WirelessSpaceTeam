2a7bcb32511c08f7b6ae768000cfa383433112d7
inherentlyMalicious <33167525+inherentlyMalicious@users.noreply.github.com>
2017-11-03 00:07:13 -0700

    minimal viable product
    what the name says

--- arduinoFinal.ino
+++ arduinoFinal.ino
@@ -30,9 +30,9 @@ String series = "";
 uint8_t len = 0;
 
 void setup() {
-  pinmode(BUTTON_G, INPUT);
-  pinmode(BUTTON_Y, INPUT);
-  pinmode(BUTTON_R, INPUT);
+  pinMode(BUTTON_G, INPUT);
+  pinMode(BUTTON_Y, INPUT);
+  pinMode(BUTTON_R, INPUT);
 
   radioA.begin();  //begin
   radioA.setChannel(COM_CHANNEL);  //set unique channel for communication
@@ -42,42 +42,54 @@ void setup() {
   radioA.setCRCLength(RF24_CRC_16);
 
   Serial.begin(9600);
+  Serial.print("Setup completed\n");
 }
 
 void loop() {
   // stop loop until bytes available to read
   radioA.startListening();
   while (!radioA.available()) {}
-  
-  // read byte from teensy
-  char newChar;
-  radioA.read(&newChar, 1);
+    Serial.print("Reading from Teensy\n");
+    // read byte from teensy
+    char newChar[] = "";
+    radioA.read(&newChar, sizeof(char));
+//    Serial.print("--->");
+//    char theChar[18];
+//    sprintf(theChar, "%s", String(newChar,DEC));
+//    Serial.print("<---");
+    series.concat(newChar);
+//    Serial.print(atoi(newChar));
+//    Serial.print('\n');
+    len++;
   radioA.stopListening();
-
-  series.concat(newChar);
-
-  // check button presses
+    
+    // check button presses
+    
+    char state[] = {0, 0, 0};  // red, yellow, green
+    uint8_t stateIcr = 0;
   
-  char state[] = {0, 0, 0};  // red, yellow, green
-  uint8_t stateIcr = 0;
-
-  String input = "";
-  uint8_t count = 0;
-  char eval = 1;
-  while (eval && (len - count)) {
-
-    for (stateIcr = 0; stateIcr < 3, stateIcr++) {
-      char currState = digitalRead(4 - stateIcr);
-
-      if (state[stateIcr] > currState) {
-      	 input.concat(colors[stateIcr]);
-	 if (input[count] != series[count])
-      	 	eval = 0;
-	 count++;
-      }
-      state[stateIcr] = currState;
-    }
-
+    String input = "";
+    uint8_t count = 0;
+    char isCorrect = 1; // correctness of current
+    while (isCorrect && (len - count)) {
+      for (stateIcr = 0; stateIcr < 3; stateIcr++) {
+        char currState = digitalRead(4 - stateIcr);
+  
+        if (state[2-stateIcr] > currState) {
+          char pressedButton[16];
+          sprintf(pressedButton, "%d was pressed\n", stateIcr);
+           input += colors[stateIcr];
+           delay(1000);
+            if (input[count] != series[count])
+              isCorrect = 0;
+            count++;
+        }
+        state[stateIcr] = currState;
+      } 
   }
-  radioA.write(&eval, 1);
+  if (isCorrect)
+    Serial.print("CORRECT");
+  else
+    Serial.print("WRONG");
+  radioA.write(&isCorrect, 1);
 }
