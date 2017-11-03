e64aeaab1175206a26ecae4a4763f6a8f11519d7
Rob Griffith <rwgriffithv@gmail.com>
2017-11-02 22:42:24 -0700

    Add files via upload

---
+++ arduinoFinal.ino
@@ -0,0 +1,82 @@
+//Arduino Code
+
+#include <stdlib.h>
+#include "RF24.h"
+#include "nRF24L01.h"
+
+#define BUTTON_R 4
+#define BUTTON_Y 3
+#define BUTTON_G 2
+
+// pins for Teensy to digitalWrite() to
+#define R 19 
+#define Y 18
+#define G 17
+
+#define CEPIN   9
+#define CSPIN   10
+#define SCK     13
+#define MOSI    11
+#define MISO    12
+
+#define COM_CHANNEL 10
+#define TEENSY_WRITE 0xE7E7E7E7E7
+#define ARDUINO_WRITE 0xC2C2C2C2C2
+
+const char colors[] = {R, Y, G};
+
+RF24 radioA = RF24(CEPIN, CSPIN); // initialize
+String series = "";
+uint8_t len = 0;
+
+void setup() {
+  pinmode(BUTTON_G, INPUT);
+  pinmode(BUTTON_Y, INPUT);
+  pinmode(BUTTON_R, INPUT);
+
+  radioT.begin();  //begin
+  radioT.setChannel(COM_CHANNEL);  //set unique channel for communication
+  radioT.setPALevel(RF24_PA_MIN);
+  radioT.openWritingPipe(ARDUINO_WRITE);
+  radioT.openReadingPipe(1, TEENSY_WRITE);
+  radioT.setCRCLength(RF24_CRC_16);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // stop loop until bytes available to read
+  radioA.startListening();
+  while (!radioA.available()) {}
+  
+  // read byte from teensy
+  char newChar;
+  radioA.read(&newChar, 1);
+  radioA.stopListening();
+
+  series += newChar;
+
+  // check button presses
+  
+  char state[] = {0, 0, 0};  // green, yellow, red
+  uint8_t stateIcr = 0;
+
+  String input = "";
+  uint8_t count = 0;
+  char eval = 1;
+  while (eval && (len - count)) {
+
+    for (stateIcr = 0; stateIcr < 3, stateIcr++) {
+      char currState = digitalRead(2 + stateIcr);
+
+      if (state[stateIcr] > currState) {
+      	 input += (stateIcr + 17);
+	 count++;
+      }
+      state[stateIcr] = currState;
+    }
+    if (input[i] != series[i])
+      eval = 0;
+  }
+  radioA.write(&eval, 1);
+}
